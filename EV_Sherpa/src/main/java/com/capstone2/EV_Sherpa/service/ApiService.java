69c7108a67cbc84906697fea1c9a3cc2130b26a9
이주환 <70500003+JHwan96@users.noreply.github.com>
2021-06-12 08:52:03 +0900

    modify ApiService

--- EV_Sherpa/src/main/java/com/capstone2/EV_Sherpa/service/ApiService.java
+++ EV_Sherpa/src/main/java/com/capstone2/EV_Sherpa/service/ApiService.java
@@ -59,37 +59,43 @@ public class ApiService {
         HtmlUtil htmlUtil = new HtmlUtil();
         int itemSize;
         String result = "[";
-        String tempResult = "";
+        String tempResult ;
         String jsonData = new String();
         List<ApiInformation> apiInformationList = new ArrayList<>();
-        org.json.JSONObject jOb;
         org.json.JSONArray item = new org.json.JSONArray();
 
+        for(int i = 0; i < 40; i++) {
+            if (i == 11 || (i > 20 && i < 30) || (i > 30 && i < 40)) {
+                String integer = Integer.toString(i);
                 StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/B552584/EvCharger/getChargerInfo"); /*URL*/
                 urlBuilder.append("?" + URLEncoder.encode("ServiceKey", "UTF-8") + "WJo08u7NjS7h%2FNNZuvLvDZssjBLtqhGdpO939Mzlh9TERxC9Q7k%2BKrxh0MJfafGGlS8NrJLhDFxcy6kVcp5upA%3D%3D"); /*Service Key*/
                 urlBuilder.append("&" + URLEncoder.encode("ServiceKey", "UTF-8") + "=" + URLEncoder.encode("WJo08u7NjS7h/NNZuvLvDZssjBLtqhGdpO939Mzlh9TERxC9Q7k+Krxh0MJfafGGlS8NrJLhDFxcy6kVcp5upA==", "UTF-8")); /*공공데이터포털에서 받은 인증키*/
                 urlBuilder.append("&" + URLEncoder.encode("pageNo", "UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
                 urlBuilder.append("&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode("9999", "UTF-8")); /*한 페이지 결과 수 (최소 10, 최대 9999)*/
-                urlBuilder.append("&" + URLEncoder.encode("zcode", "UTF-8") + "=" + URLEncoder.encode("11", "UTF-8")); /*시도 코드 (행정구역코드 앞 2자리)*/
+                urlBuilder.append("&" + URLEncoder.encode("zcode", "UTF-8") + "=" + URLEncoder.encode(integer, "UTF-8")); /*시도 코드 (행정구역코드 앞 2자리)*/
 
                 jsonData = new HtmlUtil().HtmlParser(urlBuilder.toString());
 
                 try {
-///?????
-                    jOb = new org.json.JSONObject(jsonData);
+                    org.json.JSONObject jOb = new org.json.JSONObject(jsonData);
                     org.json.JSONObject parseResponse = jOb.getJSONObject("response");
                     org.json.JSONObject parseBody = parseResponse.getJSONObject("body");
                     org.json.JSONObject parseItems = parseBody.getJSONObject("items");
                     item = parseItems.getJSONArray("item");
-                    result = item.toString().replace("]", "");
-                    result = result.replace("[","");
+                    tempResult = item.toString().replace("]", "");
+                    tempResult = tempResult.replace("[", "");
+                    result = result + tempResult;
+                    result = result + ",";
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
 
-
-
+            }
+        }
+        result = result.substring(0, result.length()-1);
+        result = result + "]";
 
         return result;
     }
 }
+
